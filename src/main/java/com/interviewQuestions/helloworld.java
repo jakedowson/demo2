5252b8a71ddf54495fe0da40b4a72ebee1d25384
jake <969128839@qq.com>
2020-10-10 17:07:25 +0800

    创建一个新的分支合并远程分支

--- src/main/java/com/interviewQuestions/helloworld.java
+++ src/main/java/com/interviewQuestions/helloworld.java
@@ -4,17 +4,6 @@ public class helloworld {
 
 
     public static void main(String[] args) {
-        System.out.println("你好我开始学git了");
-
-
-        System.out.println("我爱你");
-
-
-        System.out.println("我爱你们");
-
-        System.out.println("分支demo");
-
-
-        System.out.println("demo1");
+        System.out.println("合并远程分支");
     }
 }
